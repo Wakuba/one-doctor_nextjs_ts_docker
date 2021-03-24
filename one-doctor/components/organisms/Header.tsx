216256f0e96093ea0023cd8237c34db22195c794
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-03-25 00:26:30 +0900

    前のプロジェクトからの移行完了

---
+++ one-doctor/components/organisms/Header.tsx
@@ -0,0 +1,11 @@
+
+import styles from '../../styles/Home.module.css'
+
+export default function Header () {
+	return (
+      <header className={styles.header}>
+        <img src="/onedoctor.png" alt="One Doctor Logo" />
+        <h1>One Doctor</h1>
+      </header>
+			)
+}
--- one-doctor/pages/index.tsx
+++ one-doctor/pages/index.tsx
@@ -1,15 +1,33 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Header from '../components/organisms/Header.tsx'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>One Doctor</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default IndexPage
+			<Header/>
+
+      <main className={styles.main}>
+        <h1>OneDoctor</h1>
+        <button className={styles.button}>おしてみそ</button>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/onedoctor.png" alt="One Doctor Logo" className={styles.logo} />
+          One Doctor
+        </a>
+      </footer>
+    </div>
+  )
+}
