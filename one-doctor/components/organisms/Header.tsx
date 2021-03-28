c08f375d51c5c109fd91932ff0a486de01bf2265
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-03-28 20:52:47 +0900

    change the favicon.ico which background is transparent

--- one-doctor/components/organisms/Header.tsx
+++ one-doctor/components/organisms/Header.tsx
@@ -3,7 +3,7 @@ import styles from '../../styles/Home.module.css'
 
 export default function Header () {
 	return (
-		<header className="flex w-full h-1/5 border-b items-baseline justify-left" >
+		<header className="flex w-full h-1/5 border-2 items-baseline justify-left" >
 			<img src="/onedoctor.png" alt="One Doctor Logo" className="h-12" />
 		</header>
 			)
