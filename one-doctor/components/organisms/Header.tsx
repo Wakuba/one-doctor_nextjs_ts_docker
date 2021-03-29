718d52c6f6bbdaf40ffce018d28b67f8d52b4492
wakuba <1insp99perspTashTash16+AengineI@gmail.com>
2021-03-29 16:53:36 +0900

    installed modal module

--- one-doctor/components/organisms/Header.tsx
+++ one-doctor/components/organisms/Header.tsx
@@ -2,10 +2,11 @@ import styles from '../../styles/Home.module.css'
 
 export default function Header () {
 	return (
-		<header className="flex w-full h-1/5 border-2 items-baseline justify-between" >
+		<header className="flex w-full h-1/5 border-2 justify-between" >
 			<img src="/onedoctor.png" alt="One Doctor Logo" className="h-12" />
-			<div>
-				<img src="/noImage.png" alt="User Image" className="h-12 mx-auto rounded-full" />
+			<div className="flex h-12 w-50 justify-start items-center" >
+				<p>MY PAGE</p>
+				<img src="/noImage.png" alt="User Image" className="h-12 rounded-full ml-3" />
 			</div>
 		</header>
 			)
